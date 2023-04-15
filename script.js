82333c1ada19c590523f9c180944e588083c3048
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-15 16:13:26 +0430

    making function for change background Color~

--- script.js
+++ script.js
@@ -12,24 +12,25 @@ const Colors = [
   "Plum",
 ];
 
-
-
 /// Making buttons for each color and add event to button for change backgroundColor of body
 Colors.forEach((color) => {
   const button = document.createElement("button");
   button.style.backgroundColor = color;
   button.onclick = function () {
-    document.body.style.backgroundColor = color;
-    document.querySelector("#current-color").innerHTML = color;
+    setBackgroundColor(color);
   };
   document.querySelector("div.colors").appendChild(button);
 });
 
-
-
 /// making random background color for body
-document.body.onload = function(){
-    const randomIndex = Math.floor(Math.random()*Colors.length)
-    document.querySelector("#current-color").innerHTML = Colors[randomIndex]
-    document.body.style.backgroundColor = Colors[randomIndex]
+document.body.onload = function () {
+  const randomIndex = Math.floor(Math.random() * Colors.length);
+  const color = Colors[randomIndex];
+  setBackgroundColor(color);
+};
+
+/// making function for change background Color
+function setBackgroundColor(color) {
+  document.querySelector("#current-color").innerHTML = color;
+  document.body.style.backgroundColor = color;
 }
