71dc47ad5b956c4e90dbbd02b5e342324c70f762
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-15 16:39:00 +0430

    making function for change background Color and add active class to activeButton

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ const Colors = [
   "Plum",
 ];
 
+/// making function for change background Color and add active class to activeButton
+function setBackgroundColor(color) {
+  document.querySelector("#current-color").innerHTML = color;
+  document.body.style.backgroundColor = color;
+  const currntActiveButton = document.querySelector("button.active")
+  if (currntActiveButton !== null) {
+    currntActiveButton.classList.remove("active")
+    
+  }
+  document.querySelector(`button[data-color = ${color}]`).classList.add("active")
+}
+
 /// Making buttons for each color and add event to button for change backgroundColor of body
 Colors.forEach((color) => {
   const button = document.createElement("button");
@@ -19,6 +31,7 @@ Colors.forEach((color) => {
   button.onclick = function () {
     setBackgroundColor(color);
   };
+  button.setAttribute(`data-color`, `${color}`);
   document.querySelector("div.colors").appendChild(button);
 });
 
@@ -28,9 +41,3 @@ document.body.onload = function () {
   const color = Colors[randomIndex];
   setBackgroundColor(color);
 };
-
-/// making function for change background Color
-function setBackgroundColor(color) {
-  document.querySelector("#current-color").innerHTML = color;
-  document.body.style.backgroundColor = color;
-}
