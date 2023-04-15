c6a7723b6567f8bfad5b806dbc6df2e3978a1b47
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-15 16:00:01 +0430

    Making buttons for each color and add event to button for change backgroundColor of body

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+/// create array of colors
 const Colors = [
- "LightSteelBlue" ,
- "Coral",
- "Crimson",
- "DarkKhaki",
- "DarkOrange",
- "Gold",
- "LightPink",
- "LightGreen",
- "Olive",
- "Plum",
+  "LightSteelBlue",
+  "Coral",
+  "Crimson",
+  "DarkKhaki",
+  "DarkOrange",
+  "Gold",
+  "LightPink",
+  "LightGreen",
+  "Olive",
+  "Plum",
 ];
 
-
-
+/// Making buttons for each color and add event to button for change backgroundColor of body
+Colors.forEach((color) => {
+  const button = document.createElement("button");
+  button.style.backgroundColor = color;
+  button.onclick = function () {
+    document.body.style.backgroundColor = color;
+    document.querySelector("#current-color").innerHTML = color;
+  };
+  document.querySelector("div.colors").appendChild(button);
+});
